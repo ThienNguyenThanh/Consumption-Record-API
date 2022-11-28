076eff4abfdba9bb796ab7de6eca05b9dd8b68a8
thien <s3817852@rmit.edu.vn>
2022-11-28 10:31:13 +0700

    add header to consump-read-all

--- functions/consump-read-all.js
+++ functions/consump-read-all.js
@@ -41,6 +41,10 @@ exports.handler = (event, context, callback) =>{
     ).then((response) => {
         return  {
             statusCode: 200,
+            headers:{
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+            },
             body: JSON.stringify(response)
         }
     }).catch((error) => {
